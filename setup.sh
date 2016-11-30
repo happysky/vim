66d8461415dd4fb82bf9820ce944bd46a3835454
wuxupeng <wuxupeng_1314@163.com>
2016-11-30 15:48:52 +0800

    配置文件修改

--- setup.sh
+++ setup.sh
@@ -14,12 +14,10 @@ fi
 
 sudo easy_install -ZU autopep8 
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
-mv -f ~/vim ~/vim_old
-cd ~/ && git clone https://github.com/ma6174/vim.git
 mv -f ~/.vim ~/.vim_old
-mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
-mv -f ~/.vim/.vimrc ~/
+cd ~/ && git clone git@github.com:happysky/vim.git ./.vim
+ln -s ~/.vim/.vimrc ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo "ma6174正在努力为您安装bundle程序" > ma6174
 echo "安装完毕将自动退出" >> ma6174
